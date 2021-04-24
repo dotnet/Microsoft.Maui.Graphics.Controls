0a8654fd27b611eabf1b7be55af32f861aad0f15
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-04-24 20:57:37 +0200

    Register graphics handlers

--- src/GraphicsControls.Sample/Startup.cs
+++ src/GraphicsControls.Sample/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Maui.Controls.Compatibility;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Maui.Controls.Compatibility;
 using Microsoft.Maui.Hosting;
 
 namespace Microsoft.Maui.Graphics.Controls
@@ -7,8 +9,17 @@ namespace Microsoft.Maui.Graphics.Controls
 	{
 		public void Configure(IAppHostBuilder appBuilder)
 		{
+	
 			appBuilder
 				.UseFormsCompatibility()
+				.ConfigureMauiHandlers((_, handlersCollection) => handlersCollection.AddHandlers(new Dictionary<Type, Type>
+				{
+					{ typeof(ICheckBox), typeof(CheckBoxHandler) },
+					{ typeof(IProgress), typeof(ProgressBarHandler) },
+					{ typeof(ISlider), typeof(SliderHandler) },
+					{ typeof(IStepper), typeof(StepperHandler) },
+					{ typeof(ISwitch), typeof(SwitchHandler) }
+				}))
 				.UseMauiApp<MyApp>();
 		}
 	}
