7538568b1258a263df8c15d16b538bbcf56d8a09
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-08-06 11:29:08 +0200

    Created GraphicsControls AppHostBuilderExtensions

--- src/GraphicsControls.Sample/GraphicsControls.Sample/Startup.cs
+++ src/GraphicsControls.Sample/GraphicsControls.Sample/Startup.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.Maui;
-using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Hosting;
-using Microsoft.Maui.Graphics.Controls;
 using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Graphics.Controls.Hosting;
 
 namespace GraphicsControls.Sample
 {
@@ -12,19 +11,7 @@ namespace GraphicsControls.Sample
         {
             appBuilder
                 .UseMauiApp<App>()
-                .ConfigureMauiHandlers(handlers =>
-                {
-                    handlers.AddHandler(typeof(Button), typeof(ButtonHandler));
-                    handlers.AddHandler(typeof(CheckBox), typeof(CheckBoxHandler));
-                    handlers.AddHandler(typeof(DatePicker), typeof(DatePickerHandler));
-                    handlers.AddHandler(typeof(Editor), typeof(EditorHandler));
-                    handlers.AddHandler(typeof(Entry), typeof(EntryHandler));
-                    handlers.AddHandler(typeof(ProgressBar), typeof(ProgressBarHandler));
-                    handlers.AddHandler(typeof(Slider), typeof(SliderHandler));
-                    handlers.AddHandler(typeof(Stepper), typeof(StepperHandler));
-                    handlers.AddHandler(typeof(Switch), typeof(SwitchHandler));
-                    handlers.AddHandler(typeof(TimePicker), typeof(TimePickerHandler));
-                });
+                .ConfigureGraphicsControls();
         }
     }
 }
---
+++ src/GraphicsControls/Hosting/AppHostBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Hosting;
+
+namespace Microsoft.Maui.Graphics.Controls.Hosting
+{
+    public static class AppHostBuilderExtensions
+    {
+        public static IAppHostBuilder ConfigureGraphicsControls(this IAppHostBuilder builder)
+        {
+            builder.ConfigureMauiHandlers(handlers =>
+             {
+                 handlers.AddHandler(typeof(Button), typeof(ButtonHandler));
+                 handlers.AddHandler(typeof(CheckBox), typeof(CheckBoxHandler));
+                 handlers.AddHandler(typeof(DatePicker), typeof(DatePickerHandler));
+                 handlers.AddHandler(typeof(Editor), typeof(EditorHandler));
+                 handlers.AddHandler(typeof(Entry), typeof(EntryHandler));
+                 handlers.AddHandler(typeof(ProgressBar), typeof(ProgressBarHandler));
+                 handlers.AddHandler(typeof(Slider), typeof(SliderHandler));
+                 handlers.AddHandler(typeof(Stepper), typeof(StepperHandler));
+                 handlers.AddHandler(typeof(Switch), typeof(SwitchHandler));
+                 handlers.AddHandler(typeof(TimePicker), typeof(TimePickerHandler));
+             });
+
+            return builder;
+        }
+	}
+}
