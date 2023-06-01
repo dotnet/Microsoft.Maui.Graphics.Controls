d41bdc2ec66bc377a53ac27d50e7368af8906eea
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-06-01 11:02:53 +0200

    Implement drawn Label control

---
+++ src/GraphicsControls/Handlers/Label/ILabelDrawable.cs
@@ -0,0 +1,8 @@
+﻿namespace Microsoft.Maui.Graphics.Controls
+{
+    public interface ILabelDrawable : IViewDrawable<ILabel>
+    {
+        void DrawBackground(ICanvas canvas, RectF dirtyRect, ILabel view);
+        void DrawText(ICanvas canvas, RectF dirtyRect, ILabel view);
+    }
+}
---
+++ src/GraphicsControls/Platform/TextExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace Microsoft.Maui.Graphics.Controls
+{
+    public static class TextExtensions
+    {
+        public static string WithCharacterSpacing(this string input, double characterSpacing)
+        {
+            int spaces = (int)characterSpacing;
+
+            if (input.Length <= 1)
+                return input;
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                sb.Append(input[i]);
+                if (i != input.Length - 1)
+                    sb.Append(' ', spaces);
+            }
+
+            return sb.ToString();
+        }
+
+        public static HorizontalAlignment ToHorizontalAlignment(this TextAlignment textAlignment)
+        {
+            switch (textAlignment)
+            {
+                case TextAlignment.Start:
+                    return HorizontalAlignment.Left;
+                case TextAlignment.Center:
+                    return HorizontalAlignment.Center;
+                case TextAlignment.End:
+                    return HorizontalAlignment.Right;
+                default:
+                    return HorizontalAlignment.Left;
+            }
+        }
+
+        public static VerticalAlignment ToVerticalAlignment(this TextAlignment textAlignment)
+        {
+            switch (textAlignment)
+            {
+                case TextAlignment.Start:
+                    return VerticalAlignment.Top;
+                case TextAlignment.Center:
+                    return VerticalAlignment.Center;
+                case TextAlignment.End:
+                    return VerticalAlignment.Bottom;
+                default:
+                    return VerticalAlignment.Top;
+            }
+        }
+    }
+}
