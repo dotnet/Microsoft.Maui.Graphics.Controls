d6a129ea353d3681c018c3eb5d16dfaa7f33224e
Gordian <68512769+Gordiancyber@users.noreply.github.com>
2023-10-29 03:14:43 +0100

    Adding comments to Update CheckBoxHandler.cs
    Adding comments to explain the purpose of each section and method in the CheckBoxHandler class, makes it easier to understand its functionality.

--- src/GraphicsControls/Handlers/CheckBox/CheckBoxHandler.cs
+++ src/GraphicsControls/Handlers/CheckBox/CheckBoxHandler.cs
@@ -1,73 +1,84 @@
-﻿using System.Linq;
+using System.Linq;
 
 namespace Microsoft.Maui.Graphics.Controls
 {
-	public class CheckBoxHandler : GraphicsControlHandler<ICheckBoxDrawable, ICheckBox>
-	{
-		readonly DrawableType _drawableType;
+    public class CheckBoxHandler : GraphicsControlHandler<ICheckBoxDrawable, ICheckBox>
+    {
+        readonly DrawableType _drawableType;
 
-		public CheckBoxHandler() : base(DrawMapper, PropertyMapper)
-		{
+        // Constructor for CheckBoxHandler with no arguments
+        public CheckBoxHandler() : base(DrawMapper, PropertyMapper)
+        {
+        }
 
-		}
+        // Constructor for CheckBoxHandler with DrawableType argument
+        public CheckBoxHandler(DrawableType drawableType) : base(DrawMapper, PropertyMapper)
+        {
+            _drawableType = drawableType;
+        }
 
-		public CheckBoxHandler(DrawableType drawableType) : base(DrawMapper, PropertyMapper)
-		{
-			_drawableType = drawableType;
-		}
+        // PropertyMapper for ICheckBox properties
+        public static PropertyMapper<ICheckBox> PropertyMapper = new PropertyMapper<ICheckBox>(ViewHandler.Mapper)
+        {
+            [nameof(ICheckBox.IsChecked)] = ViewHandler.MapInvalidate,
+            [nameof(ICheckBox.Foreground)] = ViewHandler.MapInvalidate,
+        };
 
-		public static PropertyMapper<ICheckBox> PropertyMapper = new PropertyMapper<ICheckBox>(ViewHandler.Mapper)
-		{
-			[nameof(ICheckBox.IsChecked)] = ViewHandler.MapInvalidate,
-			[nameof(ICheckBox.Foreground)] = ViewHandler.MapInvalidate,
-		};
+        // DrawMapper for ICheckBoxDrawable and ICheckBox
+        public static DrawMapper<ICheckBoxDrawable, ICheckBox> DrawMapper = new DrawMapper<ICheckBoxDrawable, ICheckBox>(ViewHandler.DrawMapper)
+        {
+            ["Background"] = MapDrawBackground,
+            ["Mark"] = MapDrawMark,
+            ["Text"] = MapDrawText
+        };
 
-		public static DrawMapper<ICheckBoxDrawable, ICheckBox> DrawMapper = new DrawMapper<ICheckBoxDrawable, ICheckBox>(ViewHandler.DrawMapper)
-		{
-			["Background"] = MapDrawBackground,
-			["Mark"] = MapDrawMark,
-			["Text"] = MapDrawText
-		};
+        // Default drawing order for CheckBox layers
+        public static string[] DefaultCheckBoxLayerDrawingOrder =
+            ViewHandler.DefaultLayerDrawingOrder.ToList().InsertAfter(new string[]
+            {
+                "Background",
+                "Mark",
+            }, "Text").ToArray();
 
-		public static string[] DefaultCheckBoxLayerDrawingOrder =
-			ViewHandler.DefaultLayerDrawingOrder.ToList().InsertAfter(new string[]
-			{
-				"Background",
-				"Mark",
-			}, "Text").ToArray();
+        // Override method to return the layer drawing order
+        public override string[] LayerDrawingOrder() =>
+            DefaultCheckBoxLayerDrawingOrder;
 
-		public override string[] LayerDrawingOrder() =>
-			DefaultCheckBoxLayerDrawingOrder;
+        // Create an ICheckBoxDrawable based on the DrawableType
+        protected override ICheckBoxDrawable CreateDrawable()
+        {
+            switch (_drawableType)
+            {
+                default:
+                case DrawableType.Material:
+                    return new MaterialCheckBoxDrawable();
+                case DrawableType.Cupertino:
+                    return new CupertinoCheckBoxDrawable();
+                case DrawableType.Fluent:
+                    return new FluentCheckBoxDrawable();
+            }
+        }
 
-		protected override ICheckBoxDrawable CreateDrawable()
-		{
-			switch (_drawableType)
-			{
-				default:
-				case DrawableType.Material:
-					return new MaterialCheckBoxDrawable();
-				case DrawableType.Cupertino:
-					return new CupertinoCheckBoxDrawable();
-				case DrawableType.Fluent:
-					return new FluentCheckBoxDrawable();
-			}
-		}
+        // Method to draw the background of the CheckBox
+        public static void MapDrawBackground(ICanvas canvas, RectF dirtyRect, ICheckBoxDrawable drawable, ICheckBox view)
+            => drawable.DrawBackground(canvas, dirtyRect, view);
 
-		public static void MapDrawBackground(ICanvas canvas, RectF dirtyRect, ICheckBoxDrawable drawable, ICheckBox view)
-			=> drawable.DrawBackground(canvas, dirtyRect, view);
+        // Method to draw the check mark of the CheckBox
+        public static void MapDrawMark(ICanvas canvas, RectF dirtyRect, ICheckBoxDrawable drawable, ICheckBox view)
+            => drawable.DrawMark(canvas, dirtyRect, view);
 
-		public static void MapDrawMark(ICanvas canvas, RectF dirtyRect, ICheckBoxDrawable drawable, ICheckBox view)
-			=> drawable.DrawMark(canvas, dirtyRect, view);
+        // Method to draw the text of the CheckBox
+        public static void MapDrawText(ICanvas canvas, RectF dirtyRect, ICheckBoxDrawable drawable, ICheckBox view)
+            => drawable.DrawText(canvas, dirtyRect, view);
 
-		public static void MapDrawText(ICanvas canvas, RectF dirtyRect, ICheckBoxDrawable drawable, ICheckBox view)
-			=> drawable.DrawText(canvas, dirtyRect, view);
+        // Override method to handle user interaction with the CheckBox
+        public override bool StartInteraction(PointF[] points)
+        {
+            if (VirtualView != null)
+                VirtualView.IsChecked = !VirtualView.IsChecked;
 
-		public override bool StartInteraction(PointF[] points)
-		{
-			if (VirtualView != null)
-				VirtualView.IsChecked = !VirtualView.IsChecked;
-
-			return base.StartInteraction(points);
-		}
-	}
+            return base.StartInteraction(points);
+        }
+    }
 }
+
